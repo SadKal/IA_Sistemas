dd68fbda0f7566e4818a507bbe53a62c12ba02df
Alex <huertacimasa@gmail.com>
2023-05-03 19:34:22 +0200

    Añadido pooling

--- main.py
+++ main.py
@@ -34,7 +34,33 @@ class Convolucion:
       salida[i, j] = np.sum(im_region * self.filtros, axis=(1, 2))
 
     return salida
-  
+
+
+
+class MaxPool:
+  #Funcion para hacer pooling
+
+  def iterar_regiones_imagen(self, imagen):
+    h, w, _ = imagen.shape
+    new_h = h // 2
+    new_w = w // 2
+
+    for i in range(new_h):
+      for j in range(new_w):  
+        im_region = imagen[(i * 2):(i * 2 + 2), (j * 2):(j * 2 + 2)]
+        yield im_region, i, j
+
+  def pasada(self, input):
+    '''
+    Devuelve un array 3d con la mitad de tamaño del original
+    '''
+    h, w, num_filtros = input.shape
+    salida = np.zeros((h // 2, w // 2, num_filtros))
+
+    for im_region, i, j in self.iterar_regiones_imagen(input):
+      salida[i, j] = np.amax(im_region, axis=(0, 1))
+
+    return salida
 
 
 imagenes_entrenamiento = mn.train_images()
@@ -43,3 +69,13 @@ etiquetas_entrenamiento = mn.train_labels()
 conv = Convolucion(8)
 output = conv.pasada(imagenes_entrenamiento[0])
 print(output.shape)
+
+train_images = mn.train_images()
+train_labels = mn.train_labels()
+
+conv = Convolucion(8)
+pool = MaxPool()
+
+output = conv.forward(train_images[0])
+output = pool.forward(output)
+print(output.shape) # (13, 13, 8)

78cbec1837208b304277b69ab1f0ae3b95650e5b
Alex <huertacimasa@gmail.com>
2023-05-03 22:12:11 +0200

    Softmax y primera parte hechos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import mnist as mn
+from softmax import Softmax 
 
 class Convolucion:
 
@@ -62,20 +63,42 @@ class MaxPool:
 
     return salida
 
+def pasada(imagen, etiqueta):
+  '''
+  Hace una pasada y saca los porcentajes
+  '''
+  # Se pasa la imagen de 0-255 a -0.5 - 0.5. Por lo que se ve es costumbre hacerlo
+  salida = conv.pasada((imagen / 255) - 0.5)
+  salida = pool.pasada(salida)
+  salida = softmax.pasada(salida)
 
-imagenes_entrenamiento = mn.train_images()
-etiquetas_entrenamiento = mn.train_labels()
+  # Calcula la perdida y la precision
+  perdida = -np.log(salida[etiqueta])
+  if (np.argmax(salida) == etiqueta):
+    precision=1
+  else:
+    precision=0
 
-conv = Convolucion(8)
-output = conv.pasada(imagenes_entrenamiento[0])
-print(output.shape)
+  return salida, perdida, precision
 
-train_images = mn.train_images()
-train_labels = mn.train_labels()
+conv = Convolucion(8)                  
+pool = MaxPool()                  
+softmax = Softmax(13 * 13 * 8, 10) 
 
-conv = Convolucion(8)
-pool = MaxPool()
+imagenes = mn.test_images()[:1000]
+etiquetas = mn.test_labels()[:1000]
 
-output = conv.pasada(train_images[0])
-output = pool.pasada(output)
-print(output.shape) # (13, 13, 8)
+perdida = 0
+num_correctos = 0
+#Enumerate hace que los valores de i sean los de un array
+#Zip hace que dos arrays se combinen---> [1,2] y [a,b] pasarian a ser [(1,a), (2,b)]
+for i, (im, etiqueta) in enumerate(zip(imagenes, etiquetas)):
+  _, l, precision = pasada(im, etiqueta)
+  perdida += l
+  num_correctos += precision
+
+  # Print stats every 100 steps.
+  if i % 100 == 99:
+    print('[Paso %d] Ultimos 100 pasos: Perdida media: %.3f | Precision: %d%%' % (i + 1, perdida / 100, num_correctos))
+    perdida = 0
+    num_correctos = 0
